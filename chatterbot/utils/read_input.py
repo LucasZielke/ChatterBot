e6c6ee845839a638f636fac10f41b9a6da8703c2
Lucas Zielke <LucasZielke@users.noreply.github.com>
2015-12-14 01:33:24 -0500

    Update read_input.py
    Whoops, what i meant to write

--- chatterbot/utils/read_input.py
+++ chatterbot/utils/read_input.py
@@ -1,14 +1,14 @@
 import sys
 
 
-def input_function(user_input = None):
+def input_function(prompt = None):
     """
     Normalize reading input between python 2 and 3.
     'raw_input' is just 'input' in python3
     """
-    if user_input = None:
-        if sys.version_info[0] < 3:
-            user_input = str(raw_input())
-        else:
-            user_input = input()
+
+    if sys.version_info[0] < 3:
+        user_input = str(raw_input(prompt))
+    else:
+        user_input = input(prompt)
     return user_input
