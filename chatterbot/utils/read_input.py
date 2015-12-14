48f4713166f8add37c64263665a82f627b3c8108
Lucas Zielke <LucasZielke@users.noreply.github.com>
2015-12-14 01:28:21 -0500

    Update read_input.py
    Allow directly inputting

--- chatterbot/utils/read_input.py
+++ chatterbot/utils/read_input.py
@@ -1,13 +1,14 @@
 import sys
 
 
-def input_function():
+def input_function(user_input = None):
     """
     Normalize reading input between python 2 and 3.
     'raw_input' is just 'input' in python3
     """
-    if sys.version_info[0] < 3:
-        user_input = str(raw_input())
-    else:
-        user_input = input()
+    if user_input = None:
+        if sys.version_info[0] < 3:
+            user_input = str(raw_input())
+        else:
+            user_input = input()
     return user_input
